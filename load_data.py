421407910c74c02ec770995b0c13271a3300f8e2
Rahul Jha <theslytherin@yandex.com>
2023-05-16 14:15:52 +0530

    added the ability to load split datasets

--- load_data.py
+++ load_data.py
@@ -10,7 +10,15 @@ import custom_dataset_script
 def load_dataset(name: str, anno_path : str, img_path : str, load_split : bool = False, split_path : str = '', extension : str = 'jpg') -> fo.Dataset:
     ''' Loads VOC dataset and returns a dataset object. Need to specify directory containing
     data in VOC format, along with annotation path and image set path.  
-    can optionally specify a list of images to load only them.'''
+    Can be used to load splits when the file is of the following format:
+    2011_003184
+    2011_003187
+    2011_003188
+    2011_003192
+    2011_003194
+    2011_003216
+    2011_003223    
+    .'''
     if not load_split:
         dataset = fo.Dataset.from_dir(
             dataset_type=fo.types.VOCDetectionDataset,
@@ -20,7 +28,7 @@ def load_dataset(name: str, anno_path : str, img_path : str, load_split : bool =
         )
         return dataset
     else:
-        filenames = [f'{os.getcwd()}/{img_path}/{i.strip()}.{extension}' for i in open(split_path, 'r').readlines()]
+        filenames = [f'{img_path}/{i.strip()}.{extension}' for i in open(split_path, 'r').readlines()]
         dataset = fo.Dataset.from_dir(
             dataset_type=fo.types.VOCDetectionDataset,
             data_path=img_path,
@@ -30,11 +38,40 @@ def load_dataset(name: str, anno_path : str, img_path : str, load_split : bool =
         # dataset.filter_field()
         # samples = []
         for sample in dataset:
-            if (sample.filepath in filenames):
+            if (sample.filepath not in filenames):
                 dataset.delete_samples(sample)
                 # samples.append(samples)
 
         return dataset
+    
+
+def load_dataset_subset(name: str, anno_path : str, img_path : str, split_path : str = '', extension : str = 'jpg') -> fo.Dataset:
+    ''' Loads VOC dataset and returns a dataset object. Need to specify directory containing
+    data in VOC format, along with annotation path and image set path.  
+    Can be used to load splits when the file is of the following format:
+    2011_003184 -1
+    2011_003187  1
+    2011_003188  2
+    2011_003192 -1
+    2011_003194  2  
+    .'''
+    # valid_files = []
+    # for i in open(split_path, 'r').readlines():
+    #     if i.strip().split(" ")[-1] != "-1":
+    #         valid_files.append(i.strip().split(" ")[0])
+    filenames = [f'{img_path}/{i.strip().split(" ")[0]}.{extension}' for i in open(split_path, 'r').readlines() if i.strip().split(" ")[-1] != "-1"]
+    dataset = fo.Dataset.from_dir(
+        dataset_type=fo.types.VOCDetectionDataset,
+        data_path=img_path,
+        labels_path=anno_path,
+        name=name
+    )
+
+    cnt = 0;
+    for sample in dataset:
+        if (sample.filepath not in filenames):
+            dataset.delete_samples(sample)
+    return dataset
 
 def convert_dataset(dataset : fo.Dataset, label_field : str, export_dir : str, dataset_type : fo.types = fo.types.COCODetectionDataset) -> None:
     '''
@@ -67,7 +104,8 @@ def train_coco(batch_size : int, input_shape : int, data_name : str, lr_decay_st
 
 if __name__ == '__main__':
     # broken_dataset = load_dataset("bloodcells", "BCCD/Annotations", "BCCD/JPEGImages", True, "BCCD/ImageSets/Main/train.txt")
-        # convert_dataset(broken_dataset, 'ground_truth', 'exported')
-    train_coco(8, 512, 'output.json', 20, 1, 8)
-        # sesh = fo.launch_app(broken_dataset)
-    # sesh.wait(-1)
+    # convert_dataset(broken_dataset, 'ground_truth', 'exported')
+    # sesh = fo.launch_app(broken_dataset)
+    ds = load_dataset_subset('dogiss', '/home/juggernautjha/Desktop/Msense/complete_training_pipeline/Datasets/VOC2012/Annotations', '/home/juggernautjha/Desktop/Msense/complete_training_pipeline/Datasets/VOC2012/JPEGImages', '../Datasets/VOC2012/ImageSets/Main/dog_train.txt')
+    sesh = fo.launch_app(ds)
+    sesh.wait(-1)
